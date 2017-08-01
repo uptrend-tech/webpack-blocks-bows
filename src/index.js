9d7025118f34301e22d36a4c946ec2da9c40b02a
Brandon Orther <brandon@omt.tech>
2017-08-01 13:21:03 -0700

    Refactor to use post function .

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 // @flow
 import type { Options } from './types';
 
+const postHook = context => context.bows || {};
 /**
  * Returns a webpack block that injects bows global `log(...)` function.
  */
 const bows = (options: Options): Function => {
   const { exclude = /\/node_modules\// } = options || {};
 
-  return context => ({
-    module: {
-      loaders: [
-        {
-          test: context.fileType('application/javascript'),
-          exclude: Array.isArray(exclude) ? exclude : [exclude],
-          loaders: ['bows-loader'],
-        },
-      ],
-    },
-  });
+  const bowsFunction = context => {
+    context.bows = {
+      module: {
+        loaders: [
+          {
+            test: context.fileType('application/javascript'),
+            exclude: Array.isArray(exclude) ? exclude : [exclude],
+            loaders: ['bows-loader'],
+          },
+        ],
+      },
+    };
+
+    return {};
+  };
+
+  return Object.assign(bowsFunction, { post: postHook });
 };
 
 module.exports = bows;
--- test/index.js
+++ test/index.js
@@ -1,37 +1,61 @@
 import bows from '../src';
 
 describe('bows', () => {
-  const context = {
+  const getContext = () => ({
     fileType: () => '*.js',
-  };
+  });
 
   it('returns webpack block with passed string exclude', () => {
-    const block = bows({ exclude: 'foo' })(context);
-    expect(block.module.loaders[0].exclude).toEqual(['foo']);
+    const context = getContext();
+    const block = bows({ exclude: 'foo' });
+    expect(block(context)).toEqual({});
+    expect(block.post(context).module.loaders[0].exclude).toEqual(['foo']);
   });
 
   it('returns webpack block with passed regex exclude', () => {
-    const block = bows({ exclude: [/bar/] })(context);
-    expect(block.module.loaders[0].exclude).toEqual([/bar/]);
+    const context = getContext();
+    const block = bows({ exclude: [/bar/] });
+    expect(block(context)).toEqual({});
+    expect(block.post(context).module.loaders[0].exclude).toEqual([/bar/]);
   });
 
   it('returns webpack block with passed array of strings exclude', () => {
-    const block = bows({ exclude: ['foo', 'bar'] })(context);
-    expect(block.module.loaders[0].exclude).toEqual(['foo', 'bar']);
+    const context = getContext();
+    const block = bows({ exclude: ['foo', 'bar'] });
+    expect(block(context)).toEqual({});
+    expect(block.post(context).module.loaders[0].exclude).toEqual([
+      'foo',
+      'bar',
+    ]);
   });
 
   it('returns webpack block with passed array of regex exclude', () => {
-    const block = bows({ exclude: [/foo/, /bar/] })(context);
-    expect(block.module.loaders[0].exclude).toEqual([/foo/, /bar/]);
+    const context = getContext();
+    const block = bows({ exclude: [/foo/, /bar/] });
+    expect(block(context)).toEqual({});
+    expect(block.post(context).module.loaders[0].exclude).toEqual([
+      /foo/,
+      /bar/,
+    ]);
   });
 
   it('returns webpack block with passed array of strgin & regex exclude', () => {
-    const block = bows({ exclude: [/foo/, 'bar'] })(context);
-    expect(block.module.loaders[0].exclude).toEqual([/foo/, 'bar']);
+    const context = getContext();
+    const block = bows({ exclude: [/foo/, 'bar'] });
+    expect(block(context)).toEqual({});
+    expect(block.post(context).module.loaders[0].exclude).toEqual([
+      /foo/,
+      'bar',
+    ]);
   });
 
   it('returns webpack block with default exclude', () => {
-    const block = bows()(context);
-    expect(block.module.loaders[0].exclude).toEqual([/\/node_modules\//]);
+    const context = getContext();
+    const block = bows();
+    expect(block(context)).toEqual({});
+    expect(block.post(context).module.loaders[0].exclude).toEqual([
+      /\/node_modules\//,
+    ]);
   });
 });
+
